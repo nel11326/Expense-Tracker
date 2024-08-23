2df51a5f4ac37f01ba6747bd2abc4ff0dd88c4b9
nel11326 <lenor11326@hotmail.com>
2024-08-23 16:40:01 +0800

    styles(update): imporving app layout and styles

---
+++ components/ExpensesOutput/ExpensesList.js
@@ -0,0 +1,17 @@
+import { FlatList, Text } from "react-native";
+
+function renderExpensesItem(itemData) {
+  return <Text>{itemData.item.description}</Text>;
+}
+
+function ExpensesList({ expenses }) {
+  return (
+    <FlatList
+      data={expenses}
+      renderItem={renderExpensesItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+}
+
+export default ExpensesList;
---
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,44 @@
+import { StyleSheet, View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../../constants/styles";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    description: "A pair of shoes",
+    amount: 59.99,
+    date: new Date("2022-12-12"),
+  },
+  {
+    id: "e2",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2022-12-31"),
+  },
+  {
+    id: "e3",
+    description: "Some Bananas",
+    amount: 89.29,
+    date: new Date("2023-1-12"),
+  },
+];
+
+function ExpensesOutput({ expenses, expensesPeriod }) {
+  return (
+    <View style={styles.container}>
+      <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+      <ExpensesList expenses={DUMMY_EXPENSES} />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary700,
+  },
+});
