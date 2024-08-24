17d04875093aa883182bd7ed53d621ed390588b8
nel11326 <lenor11326@hotmail.com>
2024-08-24 18:45:52 +0800

    input(update): setting the form layout

--- components/ManageExpense/Input.js
+++ components/ManageExpense/Input.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
-function Input({ label, textInputConfig }) {
+function Input({ style, label, textInputConfig }) {
   const inputStyles = [styles.input];
   if (textInputConfig && textInputConfig.multiline) {
     inputStyles.push(styles.inputMultiline);
   }
 
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, style]}>
       <Text style={styles.label}>{label}</Text>
       <TextInput {...textInputConfig} style={inputStyles} />
     </View>
