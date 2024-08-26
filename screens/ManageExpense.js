3b42892e0566eb2c1647e18342d382bcc910a464
nel11326 <lenor11326@hotmail.com>
2024-08-26 14:16:37 +0800

    http(update): update the post request function and add an update and delete function for expenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -4,7 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-contex";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
-import { storeExpense } from "../util/http";
+import { deleteExpense, storeExpense, updateExpense } from "../util/http";
 
 function ManageExpense({ route, navigation }) {
   const expensesCtx = useContext(ExpensesContext);
@@ -21,7 +21,8 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  function deleteExpenseHandler() {
+  async function deleteExpenseHandler() {
+    await deleteExpense(editedExpenseId);
     expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
@@ -30,12 +31,13 @@ function ManageExpense({ route, navigation }) {
     navigation.goBack();
   }
 
-  function confirmHandler(expenseData) {
+  async function confirmHandler(expenseData) {
     if (isEditing) {
       expensesCtx.updateExpense(editedExpenseId, expenseData);
+      await updateExpense(editedExpenseId, expenseData);
     } else {
-      storeExpense(expenseData);
-      expensesCtx.addExpense(expenseData);
+      const id = await storeExpense(expenseData);
+      expensesCtx.addExpense({ ...expenseData, id: id });
     }
     navigation.goBack();
   }
--- util/http.js
+++ util/http.js
@@ -3,8 +3,10 @@ import axios from "axios";
 const baseURL =
   "https://react-native-expense-tra-212b6-default-rtdb.firebaseio.com/";
 
-export function storeExpense(expenseData) {
-  axios.post(`${baseURL}/expenses.json`, expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(`${baseURL}/expenses.json`, expenseData);
+  const id = response.data.name;
+  return id;
 }
 
 export async function fetchExpenses() {
@@ -13,13 +15,22 @@ export async function fetchExpenses() {
   const expenses = [];
 
   for (const key in response.data) {
+    const expenseData = response.data[key];
     const expenseObj = {
       id: key,
-      amount: response.data[key].amount,
-      date: new Date(response.data[key].date),
-      description: response.data[key].description,
+      amount: expenseData.amount,
+      date: new Date(expenseData.date),
+      description: expenseData.description,
     };
     expenses.push(expenseObj);
   }
   return expenses;
+}
+
+export function updateExpense(id, expenseData) {
+  return axios.put(`${baseURL}/expenses/${id}.json`, expenseData);
+}
+
+export function deleteExpense(id) {
+  return axios.delete(`${baseURL}/expenses/${id}.json`);
 }
